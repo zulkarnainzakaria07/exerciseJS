aca7bf625a7f6a092830390088a07fd4a2bc91e5
zulkarnainzakaria07 <zulkarnainzakaria07@guru.smk.belajar.id>
2024-09-23 14:05:46 +0800

    exercise 1

---
+++ exercise1.js
@@ -0,0 +1,7 @@
+//buatlah sebuah function untuk mencetak nama lengkap anda
+function fullName(){
+    let nama = "Zulkarnain Zakaria";
+    //return "Nama Saya : " + nama;
+    // return "Nama Saya : ", nama;
+    return `Nama saya : ${nama}`;
+}
