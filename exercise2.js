93d7006549bd4d3c1b059581dc09b96922ba7e31
zulkarnainzakaria07 <zulkarnainzakaria07@guru.smk.belajar.id>
2024-09-23 14:25:48 +0800

    exercise 2

---
+++ exercise2.js
@@ -0,0 +1,6 @@
+//buatlah sebuah function untuk mengkonversi dari menit ke detik
+function convert(minute){
+    result = minute * 60
+    //return result;
+    return `${minute} minutes = ${result} seconds`
+}
