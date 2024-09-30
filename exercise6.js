9bce578fd52449b352fdfa98b919b5bfca377297
zulkarnainzakaria07 <zulkarnainzakaria07@guru.smk.belajar.id>
2024-09-30 15:23:09 +0800

    exercise 5 dan 6

--- exercise6.js
+++ exercise6.js
@@ -1,1 +1,8 @@
 //buatlah sebuah fungsi untuk menjumlahkan seluruh nilai array
+function totalArr(dataArr){
+    let total = 0;
+    for (arr of dataArr){
+        total = total + arr
+    }
+    return total;
+}
